393cdeb776de062c3170374b36f522e7f3e9c9f6
Dave Watts <dave@egrove.co.uk>
2013-05-07 15:38:03 +0100

    Missing full-stop in validation message.

--- src/Valerie.Site/valerie/localisation/en-gb/core.js
+++ src/Valerie.Site/valerie/localisation/en-gb/core.js
@@ -11,6 +11,6 @@
     knockout.ModelValidationState.defaultOptions.failureMessageFormat = "There are validation errors.";
 
     defaultOptions = knockout.PropertyValidationState.defaultOptions;
-    defaultOptions.invalidEntryFailureMessage = "The value entered is invalid";
+    defaultOptions.invalidEntryFailureMessage = "The value entered is invalid.";
     defaultOptions.missingFailureMessage = "A value is required.";
 })();
