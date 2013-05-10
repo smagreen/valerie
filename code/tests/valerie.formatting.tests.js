26a740edd6b95e2e63b74a7066a7b8a8db157e7d
Dave Watts <dave@egrove.co.uk>
2013-05-10 20:38:17 +0100

    Plugged in Jasmine based testing.

---
+++ code/tests/valerie.formatting.tests.js
@@ -0,0 +1,8 @@
+﻿describe("formatting.replacePlaceholders", function() {
+    it("should return the original string if replacements are not specified", function() {
+        var originalString = "{0} {1} {2}",
+            formattedString = valerie.formatting.replacePlaceholders(originalString);
+
+        expect(formattedString).toEqual(originalString);
+    });
+});
