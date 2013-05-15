d67637da0ef41c45ace3fda0d527460282cf68af
Dave Watts <dave@egrove.co.uk>
2013-05-15 22:15:46 +0100

    Comment and nudge for testling.

--- code/tests/runnerScripts/jasmine-tap-helper.js
+++ code/tests/runnerScripts/jasmine-tap-helper.js
@@ -5,6 +5,8 @@
 
     window.onload = function () {
         jasmine.getEnv().addReporter(new jasmine.TapReporter());
+
+        // Causes a problem on IE6-8
         if(typeof window.Node === "undefined") {
             window.Node = function () {};
         }
