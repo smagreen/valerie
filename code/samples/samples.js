89d31a3a7394e56a0930db641efe6fef0e2c050a
Dave Watts <dave@egrove.co.uk>
2013-05-20 19:56:00 +0100

    Removed dead code.

--- code/samples/samples.js
+++ code/samples/samples.js
@@ -3,8 +3,6 @@ $(function () {
         sampleHtml = $("#sample-html").find("script").html().replace(whitespaceExpression, ""),
         sampleScript = $("#sample-script").find("script").html().replace(whitespaceExpression, "");
 
-    //sampleHtml = sampleHtml.substring(0, sampleHtml.length - 3);
-
     $("#sample-html").find("pre").text(sampleHtml);
     $("#sample-script-pane").find("pre").text(sampleScript);
 
