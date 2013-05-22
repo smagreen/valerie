1612e222cded1c56c452eb3e67acbdba1040d6ae
smgreen <home@steve-green.com>
2013-05-22 22:30:28 +0100

    Added . [period] as a valid date part separator as defined in IBM European Standard e.g. dd.mm.yyyy

--- code/valerie/full/valerie.converters.full.js
+++ code/valerie/full/valerie.converters.full.js
@@ -3,7 +3,7 @@
 
     var
         defaultNumericHelper = new valerie.NumericHelper(),
-        dateExpression = /^(\d\d?)(?:\-|\/)(\d\d?)(?:\-|\/)(\d\d\d\d)$/,
+        dateExpression = /^(\d\d?)(?:\-|\/|\.)(\d\d?)(?:\-|\/|\.)(\d\d\d\d)$/,
         emailExpression = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
     // Shortcuts.
         pad = valerie.formatting.pad,
